938f6b975eb0261d3e60ca6908e4a21c71a9a3a2
Mohamed Lebda <m.lebda10@gmail.com>
2023-05-01 10:29:23 +0300

    navbar update

--- src/sections/nav-bar/Navbar.jsx
+++ src/sections/nav-bar/Navbar.jsx
@@ -46,6 +46,9 @@ const Navbar = () => {
 
   const toggleLinks = ()=>{
     setShowLinks(prev=>!prev)
+    if(showColors){
+      setShowColors(false);
+    }
   }
 
   return (
@@ -61,8 +64,33 @@ const Navbar = () => {
           {data.map(item =><li key={item.id}><a href={item.link}>{item.title}</a></li>)}
         </ul>
         <button onClick={toggleLinks} className="nav__menu-btn">
-        <RxHamburgerMenu className="nav__menu-icon"/>
+        <RxHamburgerMenu className={showLinks? "nav__menu-icon rotate": "nav__menu-icon"}/>
        {showLinks && <ul className="nav__menu-mobile">
+       <button id="theme__icon-mobile" onClick={toggleColors}>
+                <IoIosColorPalette />
+              </button>
+              {showColors && (
+                <div className="theme__color">
+                  <div className="primary__theme">
+                  <img className={selectedThemeIndex === 0 ? 'active' : ''} onClick={() => handleThemeChange('#D50000', 0)} src={red} alt="red"/>
+                  <img className={selectedThemeIndex === 1 ? 'active' : ''} onClick={() => handleThemeChange('#1A237E', 1)} src={blue} alt="blue"/>
+                  <img  className={selectedThemeIndex === 2 ? 'active' : ''} onClick={() => handleThemeChange('#1B5E20', 2)} src={green} alt="green"/>
+                  <img className={selectedThemeIndex === 3 ? 'active' : ''} onClick={() => handleThemeChange('#9E9D24', 3)}src={yellow} alt="yellow"/>
+                  <img className={selectedThemeIndex === 4 ? 'active' : ''} onClick={() => handleThemeChange('hsl(var(--primary-hue), 89%, 41%)', 4)}src={purple} alt="purple"/>
+                  </div>
+                  <div className="dark-mode">
+                  <CgDarkMode className="icon"/>
+                  <label className="switch">
+                    <input type="checkbox" onChange={(e) => {
+                      handleCheckboxChange(e)
+                      handleBgThemeChange(e.target.checked)
+                      }}
+                       checked={isChecked}/>
+                    <span className="slider"></span>
+                  </label>
+                  </div>
+                </div>
+              )}
           {data.map(item =><li key={item.id}><a href={item.link}>{item.title}</a></li>)}
         </ul>}    
         </button>
